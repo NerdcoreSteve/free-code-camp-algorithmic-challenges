3f13ac60c2702be34175c5c95a930ed868d1b26f
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-12 16:52:11 -0600

    completed palindromes

--- palindrome.js
+++ palindrome.js
@@ -1,1 +1,13 @@
-module.exports = () => true
+var R = require('ramda')
+
+var palindrome_test = (string) => {
+    if(string.length < 2) return true
+    else if (R.head(string) === R.last(string)) return palindrome_test(R.slice(1, -1, string))
+    else return false
+}
+
+module.exports =
+    R.pipe(
+        R.replace(/[^A-Za-z0-9]/g, ''),
+        R.toLower,
+        palindrome_test)
