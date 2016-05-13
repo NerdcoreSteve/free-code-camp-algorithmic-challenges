a7c458dd499e19f9954dbd307d9c35685aa58de1
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-05-12 18:17:55 -0600

    finished longest word

--- longest_word.js
+++ longest_word.js
@@ -1,8 +1,10 @@
 var R = require('ramda')
 
 module.exports =
-    R.pipe(
-        R.split(' '),
-        R.reduce((longest, word) =>
-            longest.length >= word.length ? longest : word),
-        R.length)
+  R.pipe(
+    R.split(' '),
+    R.reduce(
+        (longest, word) =>
+            longest.length >= word.length ? longest : word,
+        ''),
+    R.length)
