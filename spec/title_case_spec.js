91a504403ba9c2e3c4166ca1e69f6fb25d4a9620
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-06-29 15:33:13 -0600

    title case works

--- spec/title_case_spec.js
+++ spec/title_case_spec.js
@@ -8,8 +8,9 @@ describe('title_case', () => {
     it('titleCase(\'I\'m a little tea pot\') should return \'I\'m A Little Tea Pot\'.',
         () => expect(titleCase('I\'m a little tea pot')).toEqual('I\'m A Little Tea Pot'))
 
-/*
-titleCase("sHoRt AnD sToUt") should return "Short And Stout".
-titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return "Here Is My Handle Here Is My Spout".
-*/
+    it('titleCase(\'sHoRt AnD sToUt\') should return \'Short And Stout\'.',
+        () => expect(titleCase('sHoRt AnD sToUt')).toEqual('Short And Stout'))
+
+    it('titleCase(\'HERE IS MY HANDLE HERE IS MY SPOUT\') should return \'Here Is My Handle Here Is My Spout\'.',
+        () => expect(titleCase('HERE IS MY HANDLE HERE IS MY SPOUT')).toEqual('Here Is My Handle Here Is My Spout'))
 })
--- title_case.js
+++ title_case.js
@@ -7,6 +7,7 @@ module.exports =
         R.map(
             R.pipe(
                 R.split(''),
+                R.map(R.toLower),
                 R.adjust(R.toUpper, 0),
                 R.join(''))),
         R.join(' '))
