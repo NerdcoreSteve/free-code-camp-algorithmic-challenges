307a1f33638e19bd67689c98f1ebc34f3da6e5be
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-06-29 19:27:28 -0600

    adding largest_numbers

---
+++ largest_numbers.js
@@ -0,0 +1,5 @@
+const
+    R = require('ramda')
+
+module.exports =
+    R.pipe(x => x)
---
+++ spec/largest_numbers_spec.js
@@ -0,0 +1,14 @@
+const
+    R = require('ramda')
+
+describe('title_case', () => {
+    const
+        largestOfFour = require('../largest_numbers.js')
+
+    it('largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return an array.',
+        () => expect(R.type(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]))).toEqual('Array'))
+    /*
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) should return [27,5,39,1001].
+largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]) should return [9, 35, 97, 1000000].
+    */
+})
