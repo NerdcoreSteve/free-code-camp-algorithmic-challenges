fd3b670f91e2c483d5bcfa2ec842818598ac8756
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-06-30 11:09:16 -0600

    ends_with spec finished

--- ends_with.js
+++ ends_with.js
@@ -1,2 +1,1 @@
-const tap = x => { console.log(x); return x }
 module.exports = (str, target) => (new RegExp(target + '$')).test(str)
--- spec/ends_with_spec.js
+++ spec/ends_with_spec.js
@@ -3,16 +3,23 @@ describe('title_case', () => {
         confirmEnding = require('../ends_with.js')
 
     it('confirmEnding(\'Bastian\', \'n\') should return true.',
-        () => expect(confirmEnding('Bastian', 'n')).toEqual(true))
+       () => expect(confirmEnding('Bastian', 'n')).toEqual(true))
 
     it('confirmEnding(\'Connor\', \'n\') should return false.',
-        () => expect(confirmEnding('Connor', 'n')).toEqual(false))
-    /*
+       () => expect(confirmEnding('Connor', 'n')).toEqual(false))
 
-confirmEnding(\'Walking on water and developing software from a specification are easy if both are frozen\', \'specification\') should return false.
-confirmEnding(\'He has to give me a new name\', \'name\') should return true.
-confirmEnding(\'Open sesame\', \'same\') should return true.
-confirmEnding(\'Open sesame\', \'pen\') should return false.
-confirmEnding(\'If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing\', \'mountain\') should return false.
-    */
+    it('confirmEnding(\'Walking on water and developing software from a specification are easy if both are frozen\', \'specification\') should return false.',
+       () => expect(confirmEnding('Walking on water and developing software from a specification are easy if both are frozen', 'specification')).toEqual(false))
+
+    it('confirmEnding(\'He has to give me a new name\', \'name\') should return true.',
+       () => expect(confirmEnding('He has to give me a new name', 'name')).toEqual(true))
+
+    it('confirmEnding(\'Open sesame\', \'same\') should return true.',
+       () => expect(confirmEnding('Open sesame', 'same')).toEqual(true))
+
+    it('confirmEnding(\'Open sesame\', \'pen\') should return false.',
+       () => expect(confirmEnding('Open sesame', 'pen')).toEqual(false))
+
+    it('confirmEnding(\'If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing\', \'mountain\') should return false.',
+       () => expect(confirmEnding('If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing', 'mountain')).toEqual(false))
 })
