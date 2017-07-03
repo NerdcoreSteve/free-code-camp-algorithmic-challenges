f69a296aecdf1b3d30a20afb4885bb0d024b8ec7
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-02 23:06:31 -0600

    added more tests

--- spec/truncate_string_spec.js
+++ spec/truncate_string_spec.js
@@ -3,9 +3,11 @@ describe('title_case', () => {
         truncateString = require('../truncate_string.js')
 
     it('truncateString(\'A-tisket a-tasket A green and yellow basket\', 11) should return \'A-tisket...\'.',
-        () => expect(typeof truncateString('I\'m a little tea pot')).toEqual('string'))
+        () => expect(truncateString('A-tisket a-tasket A green and yellow basket', 11)).toEqual('A-tisket...'))
+
+    it('truncateString(\'Peter Piper picked a peck of pickled peppers\', 14) should return \'Peter Piper...\'.',
+        () => expect(truncateString('Peter Piper picked a peck of pickled peppers', 14)).toEqual('Peter Piper...'))
     /*
-truncateString(\'Peter Piper picked a peck of pickled peppers\', 14) should return \'Peter Piper...\'.
 truncateString(\'A-tisket a-tasket A green and yellow basket\', \'A-tisket a-tasket A green and yellow basket\'.length) should return \'A-tisket a-tasket A green and yellow basket\'.
 truncateString(\'A-tisket a-tasket A green and yellow basket\', \'A-tisket a-tasket A green and yellow basket\'.length + 2) should return \'A-tisket a-tasket A green and yellow basket\'.
 truncateString(\'A-\', 1) should return \'A...\'.
