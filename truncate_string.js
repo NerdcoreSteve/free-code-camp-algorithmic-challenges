977c994c6ad8e717854b0c67713a62b7ea3019a1
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-04 17:19:23 -0600

    all tests pass and code looks good

--- truncate_string.js
+++ truncate_string.js
@@ -1,10 +1,13 @@
 const
     R = require('ramda'),
-    {Left, Right} = require('data.either'),
-    shortEnough = (str, n) => n >= str.length ? Left(str) : Right(str)
+    {Left, Right} = require('data.either')
+
+const
+    shortEnough = (str, n) => n >= str.length ? Left(str) : Right(str),
+    conditionalSlice = n => str => R.slice(0, n <= 3 ? n : n - 3, str)
 
 module.exports = (str, n) =>
     shortEnough(str, n)
-        .map(R.slice(0, n > 3 ? n - 3 : n))
+        .map(conditionalSlice(n))
         .map(R.concat(R.__, '...'))
         .fold(x => x, x => x)
