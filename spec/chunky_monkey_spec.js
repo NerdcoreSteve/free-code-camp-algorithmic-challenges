fde4b8c843bbd4247330e0aa711ed4daec7b0f3a
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-06 22:06:26 -0600

    tweaked eslint, added another passing test

--- spec/chunky_monkey_spec.js
+++ spec/chunky_monkey_spec.js
@@ -5,8 +5,9 @@ describe('title_case', () => {
     it('chunkArrayInGroups([\'a\', \'b\', \'c\', \'d\'], 2) should return [[\'a\', \'b\'], [\'c\', \'d\']].',
         () => expect(chunkArrayInGroups(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]))
 
+    it('chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].',
+        () => expect(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3)).toEqual([[0, 1, 2], [3, 4, 5]]))
     /*
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
