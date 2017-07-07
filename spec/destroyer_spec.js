b306e65510ab0ad4c8f0a73f2fcaf6c74322ede3
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-07 00:00:04 -0600

    all tests pass for destroyer

--- spec/destroyer_spec.js
+++ spec/destroyer_spec.js
@@ -4,10 +4,16 @@ describe('destroyer', () => {
 
     it('destroyer([1, 2, 3, 1, 2, 3], 2, 3) should return [1, 1].',
         () => expect(destroyer([1, 2, 3, 1, 2, 3], 2, 3)).toEqual([1, 1]))
-/*
-destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].
-destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].
-destroyer([2, 3, 2, 3], 2, 3) should return [].
-destroyer([\'tree\', \'hamburger\', 53], \'tree\', 53) should return [\'hamburger\']
-*/
+
+    it('destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3) should return [1, 5, 1].',
+        () => expect(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3)).toEqual([1, 5, 1]))
+
+    it('destroyer([3, 5, 1, 2, 2], 2, 3, 5) should return [1].',
+        () => expect(destroyer([3, 5, 1, 2, 2], 2, 3, 5)).toEqual([1]))
+
+    it('destroyer([2, 3, 2, 3], 2, 3) should return [].',
+        () => expect(destroyer([2, 3, 2, 3], 2, 3)).toEqual([]))
+
+    it('destroyer([\'tree\', \'hamburger\', 53], \'tree\', 53) should return [\'hamburger\']',
+        () => expect(destroyer(['tree', 'hamburger', 53], 'tree', 53)).toEqual(['hamburger']))
 })
