52163c790e410bf31c8694254345ce264f9497ea
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-08 01:27:44 -0600

    finished tests for diffArray

--- diffArray.js
+++ diffArray.js
@@ -1,7 +1,6 @@
 const
     xor = (a, b) => (a || b) && !(a && b)
 
-module.exports = (xs, ys) => xs
-    .concat(ys)
-    .filter(x => xor(xs.includes(x), ys.includes(x)))
-    .sort()
+module.exports = (xs, ys) =>
+    xs.concat(ys).filter(x =>
+        xor(xs.includes(x), ys.includes(x)))
--- spec/diffArray_spec.js
+++ spec/diffArray_spec.js
@@ -9,18 +9,20 @@ describe('diffArray', () => {
         () => expect(diffArray(['diorite', 'andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'], ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub'])).toEqual(['pink wool']))
 
     it('[\'andesite\', \'grass\', \'dirt\', \'pink wool\', \'dead shrub\'], [\'diorite\', \'andesite\', \'grass\', \'dirt\', \'dead shrub\'] should return [\'diorite\', \'pink wool\'].',
-        () => expect(diffArray(['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'], ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub'])).toEqual(['diorite', 'pink wool']))
+        () => expect(diffArray(['andesite', 'grass', 'dirt', 'pink wool', 'dead shrub'], ['diorite', 'andesite', 'grass', 'dirt', 'dead shrub']).sort()).toEqual(['diorite', 'pink wool'].sort()))
 
     it('[\'andesite\', \'grass\', \'dirt\', \'dead shrub\'], [\'andesite\', \'grass\', \'dirt\', \'dead shrub\'] should return [].',
         () => expect(diffArray(['andesite', 'grass', 'dirt', 'dead shrub'], ['andesite', 'grass', 'dirt', 'dead shrub'])).toEqual([]))
 
     it('[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].',
         () => expect(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5])).toEqual([4]))
-/*
-[1, \'calf\', 3, \'piglet\'], [1, \'calf\', 3, 4] should return [\'piglet\', 4].
 
-[], [\'snuffleupagus\', \'cookie monster\', \'elmo\'] should return [\'snuffleupagus\', \'cookie monster\', \'elmo\'].
+    it('[1, \'calf\', 3, \'piglet\'], [1, \'calf\', 3, 4] should return [\'piglet\', 4].',
+        () => expect(diffArray([1, 'calf', 3, 'piglet'], [1, 'calf', 3, 4]).sort()).toEqual(['piglet', 4].sort()))
 
-[1, \'calf\', 3, \'piglet\'], [7, \'filly\'] should return [1, \'calf\', 3, \'piglet\', 7, \'filly\'].
-*/
+    it('[], [\'snuffleupagus\', \'cookie monster\', \'elmo\'] should return [\'snuffleupagus\', \'cookie monster\', \'elmo\'].',
+        () => expect(diffArray([], ['snuffleupagus', 'cookie monster', 'elmo']).sort()).toEqual(['snuffleupagus', 'cookie monster', 'elmo'].sort()))
+
+    it('[1, \'calf\', 3, \'piglet\'], [7, \'filly\'] should return [1, \'calf\', 3, \'piglet\', 7, \'filly\'].',
+        () => expect(diffArray([1, 'calf', 3, 'piglet'], [7, 'filly']).sort()).toEqual([1, 'calf', 3, 'piglet', 7, 'filly'].sort()))
 })
