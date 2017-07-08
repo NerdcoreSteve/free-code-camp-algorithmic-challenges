feb2d5702b2cddcf3167494a1e4e833c5bb36020
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-07 19:48:43 -0600

    added diffArray

---
+++ diffArray.js
@@ -0,0 +1,4 @@
+const
+    R = require('ramda')
+
+module.exports = () => R.always([])()
---
+++ spec/diffArray_spec.js
@@ -0,0 +1,17 @@
+describe('diffArray', () => {
+    const
+        diffArray = require('../diffArray.js')
+
+    it('diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]) should return an array.',
+        () => expect(Array.isArray(diffArray('I\'m a little tea pot'))).toEqual(true))
+/*
+
+["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["pink wool"].
+["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"] should return ["diorite", "pink wool"].
+["andesite", "grass", "dirt", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"] should return [].
+[1, 2, 3, 5], [1, 2, 3, 4, 5] should return [4].
+[1, "calf", 3, "piglet"], [1, "calf", 3, 4] should return ["piglet", 4].
+[], ["snuffleupagus", "cookie monster", "elmo"] should return ["snuffleupagus", "cookie monster", "elmo"].
+[1, "calf", 3, "piglet"], [7, "filly"] should return [1, "calf", 3, "piglet", 7, "filly"].
+*/
+})
