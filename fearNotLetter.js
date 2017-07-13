5d72f74c71dd13dc3554f83cb932190f1cce10e9
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2017-07-13 01:45:47 -0600

    refactored fearNotLetter

--- fearNotLetter.js
+++ fearNotLetter.js
@@ -6,12 +6,7 @@ module.exports =
         R.split(''),
         R.map(x => x.charCodeAt(0)),
         R.sort(R.subtract),
-        xs => R.pipe(
-            R.prepend(null),
-            ys => [ys, xs],
-            R.transpose,
-            R.slice(1, -1))
-                (xs),
+        R.aperture(2),
         R.filter(xs => xs[1] - xs[0] > 1),
         R.chain(xs => R.range(xs[0] + 1, xs[1])),
         R.map(String.fromCharCode),
